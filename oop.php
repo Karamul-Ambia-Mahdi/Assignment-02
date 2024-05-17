a422cc90ecb7b5a05db726e1cc5337e61575aa12
Karamul-Ambia-Mahdi <mdmahdi45@gmail.com>
2024-05-17 06:32:03 +0600

    Task 02 Done.

--- oop.php
+++ oop.php
@@ -1,54 +1,93 @@
 <?php
 class Book {
- // TODO: Add properties as Private
- 
- public function __construct($title, $availableCopies) {
- // TODO: Initialize properties
-     }
+    // TODO: Add properties as Private
+
+    private $title;
+    private $availableCopies;
+
+    public function __construct($title, $availableCopies) {
+    // TODO: Initialize properties
+
+    $this -> title = $title;
+    $this -> availableCopies = $availableCopies;
+
+    }
 
 
-// TODO: Add getTitle method
+    // TODO: Add getTitle method
 
+    public function getTitle(){
 
- 
-
-
-
- // TODO: Add getAvailableCopies method
- 
+        return $this -> title;
+    }
 
 
 
- // TODO: Add borrowBook method
- 
+    // TODO: Add getAvailableCopies method
+
+    public function getAvailableCopies(){
+
+        return $this -> availableCopies;
+    }
 
 
+    // TODO: Add borrowBook method
 
- // TODO: Add returnBook method
+    public function borrowBook(){
+
+        if($this -> availableCopies > 0){
+            
+            $this -> availableCopies--;
+        }
+    }
+
+    // TODO: Add returnBook method
+
+    public function returnBook(){
+
+        $this -> availableCopies++;
+    }
  
 }
 
 
 class Member {
- // TODO: Add properties as Private
+    // TODO: Add properties as Private
+
+    private $name;
 
 
 
- public function __construct($name) {
-      // TODO: Initialize properties
-     }
+    public function __construct($name) {
+        // TODO: Initialize properties
+
+        $this -> name = $name;
+    }
 
 
 
- // TODO: Add getName method
+    // TODO: Add getName method
+
+    public function getName(){
+
+        return $this -> name;
+    }
+    
+    // TODO: Add borrowBook method
+    
+    public function borrowBook($book){
+
+        $book -> borrowBook();
+    }
+
+
+    // TODO: Add returnBook method
  
- // TODO: Add borrowBook method
- 
+    public function returnBook($book){
+    
+        $book -> returnBook();
+    }
 
-
-
- // TODO: Add returnBook method
- 
 }
 
 
@@ -63,19 +102,26 @@ class Member {
 // Book 1 - Name: The Great Gatsby, Available Copies: 5.
 // Book 2 - Name: To Kill a Mockingbird, Available Copies: 3.
 
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
 
 
 // TODO: Create 2 members with the following properties
 // Member 1 - Name: John Doe
 // Member 2 - Name: Jane Smith
 
-
+$member1 = new Member("John Doe");
+$member2 = new Member("Jane Smith");
 
 // TODO: Apply Borrow book method to each member
 
+$member1 -> borrowBook($book1);
+$member2 -> borrowBook($book2);
 
 
 // TODO: Print Available Copies with their names:
 
+printf("Available Copies of '%s': %d\n", $book1 -> getTitle(), $book1 -> getAvailableCopies());
+printf("Available Copies of '%s': %d\n", $book2 -> getTitle(), $book2 -> getAvailableCopies());
 
 ?>
